5a9f88200ed32816e88a3f479a3b2463db64678e
Ranjanrkm <ranjan2003krmahato12@gmail.com>
2026-05-18 11:43:16 +0530

    2nd commit

--- BASICS_MATHS/armstrong.java
+++ BASICS_MATHS/armstrong.java
@@ -1,35 +1,87 @@
 import java.util.*;
-public class armstrong{
 
-    public static boolean isArmstrong(int n){
-        // int cnt = 0;
+/*
+Problem Statement:
+Check whether a given number is an Armstrong number or not.
+
+Input Format:
+- A single integer n
+
+Output Format:
+- Print "Yes it's an armstrong number" if n is Armstrong
+- Print "No it's not a armstrong number" if n is not Armstrong
+
+Dry Run Example 1:
+Input:
+153
+
+Working:
+Digits = 3
+1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+
+Output:
+Yes it's an armstrong number
+
+Dry Run Example 2:
+Input:
+123
+
+Working:
+Digits = 3
+1^3 + 2^3 + 3^3 = 1 + 8 + 27 = 36
+
+Output:
+No it's not a armstrong number
+*/
+
+public class armstrong {
+
+    // This method checks whether the given number is Armstrong or not
+    public static boolean isArmstrong(int n) {
+        // Store original number for final comparison
         int temp = n;
-        // while(temp != 0){
-        //     cnt++;
-        //     temp/=10;
-        // }
+
+        // Count number of digits
+        // Example: 153 has 3 digits
         int cnt = String.valueOf(temp).length();
+
+        // This variable stores the sum of each digit raised to the power of total digits
         int sum = 0;
+
+        // Reset temp back to original number
         temp = n;
-        while(temp != 0){
-            int ld = temp % 10;
-            sum = sum + (int)Math.pow(ld , cnt);
-            temp/= 10;
+
+        // Extract each digit one by one from the right side
+        while (temp != 0) {
+            int ld = temp % 10; // ld = last digit
+
+            // Add digit^(number of digits) to sum
+            sum = sum + (int) Math.pow(ld, cnt);
+
+            // Remove the last digit
+            temp /= 10;
         }
-        if(sum == n) {
-            return true;
-        } else {
-        return false;
-        }
+
+        // If the calculated sum is equal to original number,
+        // then it is an Armstrong number
+        return sum == n;
     }
+
     public static void main(String args[]) {
+        // Create Scanner object to take input
         Scanner sc = new Scanner(System.in);
+
+        // Read number from user
         int n = sc.nextInt();
-        if(isArmstrong(n)) {
+
+        // Check Armstrong condition and print result
+        if (isArmstrong(n)) {
             System.out.println("Yes it's an armstrong number");
         } else {
             System.out.println("No it's not a armstrong number");
         }
+
+        // Close Scanner
         sc.close();
     }
 }
